3dcd18367f81ff024e61e72f4a75dc32324f4de9
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-20 16:27:49 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,13 +3,6 @@ print("Welcome to the secret auction program. ")
 
 bidders_List = {}
 
-
-
-
-
-
-#screen clears then next bidder
-
 #compare all bids to find highest bid and winner
 def silent_auction():
     while True:    
@@ -18,18 +11,16 @@ def silent_auction():
         other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
         bidders_List.update({name_bidder:amount_bid})
         if other_bidders == "yes":
-            os.system('clear')
-            name_bidder = input("What is your name?: ")
-            amount_bid = int(input("What is your bid?: "))
-            other_bidders = input("Are there any other bidders? Type 'yes' or 'no'. ").lower()
-            bidders_List.update({name_bidder:amount_bid})
+            os.system('cls')            #screen clears then next bidder
+            continue
         elif other_bidders == "no":
             break
         
+        
     max_bid = max(bidders_List, key=bidders_List.get)
     max_value = bidders_List[max_bid]
-    print(max_value)   
+    print(f"The winner is {max_bid} with a bid of ${max_value}. ")   
     
-    #print(f"the winner is {} with a bid of ${}. ")
+   
     
 silent_auction()
